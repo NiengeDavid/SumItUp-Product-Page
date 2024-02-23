d0d4c05bd64a5856c99329c2ce38798ae2fefbdc
NiengeDavid <97910242+NiengeDavid@users.noreply.github.com>
2024-02-23 13:49:39 +0100

    Bug fixes

--- sumitup_frontend/src/utils/remove-consoles.js
+++ sumitup_frontend/src/utils/remove-consoles.js
@@ -1,5 +1,12 @@
 export const GlobalDebug = (function () {
     var savedConsole = console;
+    var originalConsole = {
+        log: console.log,
+        info: console.info,
+        warn: console.warn,
+        error: console.error
+    };
+
     /**
     * @param {boolean} debugOn
     * @param {boolean} suppressAll
@@ -8,20 +15,27 @@ export const GlobalDebug = (function () {
       var suppress = suppressAll || false;
       if (debugOn === false) {
         // supress the default console functionality
-        console = {};
         console.log = function () {};
+        console.info = function () {};
+        console.warn = function () {};
+        console.error = function () {};
         // supress all type of consoles
         if (suppress) {
+          console.log = function () {};
           console.info = function () {};
           console.warn = function () {};
           console.error = function () {};
         } else {
-          console.info = savedConsole.info;
-          console.warn = savedConsole.warn;
-          console.error = savedConsole.error;
+          console.log = originalConsole.log;
+          console.info = originalConsole.info;
+          console.warn = originalConsole.warn;
+          console.error = originalConsole.error;
         }
       } else {
-        console = savedConsole;
+        console.log = originalConsole.log;
+        console.info = originalConsole.info;
+        console.warn = originalConsole.warn;
+        console.error = originalConsole.error;
       }
     };
 })();
